977da4f2e6dfe633054010c2f622962945e6ce23
SvitlanaZhovanik <Lanasvetcat@gmail.com>
2022-04-10 21:58:41 +0300

    Create contacts model function

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,62 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const data = await listContacts();
+  res.json({
+    message: "Success",
+    data,
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await getContactById(req.params.contactId);
+  if (!contact) {
+    res.status(404).json({
+      message: "Not found",
+    });
+  }
+  res.json({
+    message: "Success",
+    data: contact,
+  });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const newContact = await addContact(req.body);
+  res.status(201).json({
+    message: "Success",
+    data: newContact,
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const contacts = await removeContact(req.params.contactId);
+  if (contacts === null) {
+    res.status(404).json({ message: "Not found" });
+  }
+  res.json({ message: "contact deleted" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const contact = await updateContact(req.params.contactId, req.body);
+  if (!contact) {
+    res.status(404).json({
+      message: "Not found",
+    });
+  }
+  res.json({
+    message: "Success",
+    data: contact,
+  });
+});
 
-module.exports = router
+module.exports = router;
